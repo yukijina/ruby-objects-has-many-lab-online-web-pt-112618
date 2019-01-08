5b327942cc214d9ea93598cff26611e497fd7bb6
Yukiji Nakatsuka <lunarbase@outlook.com>
2019-01-08 13:52:20 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,6 +1,8 @@
 class Author  
   attr_accessor :name
   
+  @@all = []
+  
   def initialize(name)
     @name = name 
     @posts = []
@@ -13,11 +15,17 @@ class Author
   def add_post(post) 
     @posts << post
     post.artist = self
+    @@all << post
   end 
   
   def add_post_by_title(title)
     post = Post.new(title)
     @posts << post
+    @@all << post
+  end 
+  
+  def self.post_count 
+    @@all.count
   end 
   
 end 
