fe22d093743b33d21409c04b2896dfcbe0cd3f5d
Yukiji Nakatsuka <lunarbase@outlook.com>
2019-01-08 14:06:20 +0000

    Done.

---
+++ lib/author.rb
@@ -0,0 +1,32 @@
+class Author  
+  attr_accessor :name
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name 
+    @posts = []
+  end 
+  
+  def posts 
+    @posts
+  end 
+  
+  def add_post(post) 
+    @posts << post
+    post.author = self
+    @@all << post
+  end 
+  
+  def add_post_by_title(title)
+    post = Post.new(title)
+    @posts << post
+    post.author = self
+    @@all << post
+  end 
+  
+  def self.post_count 
+    @@all.count
+  end 
+  
+end 
