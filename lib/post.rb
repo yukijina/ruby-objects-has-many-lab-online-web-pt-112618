4b3df649095170213f4ca8a21e5130b76ed92290
Yukiji Nakatsuka <lunarbase@outlook.com>
2019-01-08 13:56:24 +0000

    Automatically backed up by Learn

---
+++ lib/post.rb
@@ -0,0 +1,16 @@
+class Post 
+  attr_accessor :author
+  attr_reader :title
+  @@all = []
+  
+  def initialize(title)
+    @title = title
+    @@all << self
+  end 
+  
+  def self.all 
+    @@all
+  end 
+  
+  
+end 
